6165cd106b9c77a7b14bd1957c8b949ff6c53754
Colm Seale <colm.seale@altocloud.com>
2017-04-14 10:40:01 +0100

    added minified build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 var path = require('path');
+var webpack = require('webpack');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'angular-load.js',
-    library: ['angularLoad'],
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
+module.exports = function (env) {
+  var PROD = env && env.prod;
+
+  return {
+    entry: './index.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: PROD ? 'angular-load.min.js' : 'angular-load.js',
+      library: ['angularLoad'],
+      libraryTarget: 'umd'
+    },
+    plugins: PROD ? [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false }
+      })
+    ] : [],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
           }
         }
-      }
-    ]
-  },
-  externals: {
-    angular: 'angular'
-  }
-};
+      ]
+    },
+    externals: {
+      angular: 'angular'
+    }
+  };
+}
